936c5eec02524d5393391008f509b58557285974
Valeriia Oliinyk <olijnykvaleria@gmail.com>
2020-05-26 18:55:54 +0300

    changed files

--- task-3.js
+++ task-3.js
@@ -7,7 +7,7 @@ const randomIntegerFromInterval = (min, max) => {
 const makeTransaction = transaction => {
   const delay = randomIntegerFromInterval(200, 500);
   return new Promise((resolve, reject) => {
-    let id = transaction.id;
+    const { id } = transaction;
     setTimeout(() => {
       const canProcess = Math.random() > 0.3;
       if (canProcess) {
@@ -26,10 +26,18 @@ const logError = id => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
-makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 70, amount: 150 })
+  .then(logSuccess)
+  .catch(logError);
 
-makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 71, amount: 230 })
+  .then(logSuccess)
+  .catch(logError);
 
-makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 72, amount: 75 })
+  .then(logSuccess)
+  .catch(logError);
 
-makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 73, amount: 100 })
+  .then(logSuccess)
+  .catch(logError);
